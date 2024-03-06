a4f695b8ff127c8af7b011639a2e5629999e9534
ITZ ARCTIX <144344981+teamarctix@users.noreply.github.com>
2024-03-06 16:25:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", "26850449"))
-API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
-BOT_USERNAME = getenv("BOT_USERNAME", "HirokoRobot")
-BOT_TOKEN = getenv("BOT_TOKEN", "6565729549:AAGu2-wgTtBF7WQjKUAG78cAV7RtEoZ41bc")
-OWNER_ID = int(getenv("OWNER_ID", "6109551937"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6109551937 5416887843 6236996313 5218610039").split()))
-MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Rakesh:Meena@cluster0.0tdfk9u.mongodb.net/?retryWrites=true&w=majority")
+API_ID = int(getenv("API_ID", "25603034"))
+API_HASH = getenv("API_HASH", "294a7bf4488b21609436de1cdd05c316")
+BOT_USERNAME = getenv("BOT_USERNAME", "joinaccepters_bot")
+BOT_TOKEN = getenv("BOT_TOKEN", "6302756306:AAEdWTi86exWgTt8Y4hUYu6IEOqvOTUQ0Y8")
+OWNER_ID = int(getenv("OWNER_ID", "1881720028"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1881720028 1881720028 1881720028 1881720028").split()))
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://abcd:abcd@personalproject.mxx6dgi.mongodb.net/?retryWrites=true&w=majority")
